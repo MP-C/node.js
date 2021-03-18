39ab1129d9baf434063c7dd4b0a2686447ed9c99
MP-C <mp.crcarvalho@gmail.com>
2021-03-19 00:11:49 +0100

    second node.js file - cowsay

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const cowsay = require('./package.json./cowsay');
 
-let name = "Mario"
-let campus = "Brussels"
+let name = "Mario";
+let campus = "Brussels";
 
 console.log(cowsay((name),(campus));
 
